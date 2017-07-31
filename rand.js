8abce36af56698a41bb827850bd111fd8677a87c
Madelyn Nelson <mnelso12@nd.edu>
2017-07-31 11:29:59 -0400

    added random order generator

--- rand.js
+++ rand.js
@@ -6,9 +6,42 @@ $( document ).ready(function() {
 		getName();
 	});
 
+	$("#order").on("click", function(){
+		console.log("clicked order");
+		getOrder();
+	});
+
 	function getName() {
 		var num = Math.floor(Math.random() * roster.length);
 		$("#name").text(roster[num]);		
+	}
+
+	function getOrder() {
+		var arr = shuffle(roster); 
+		var str = "";
+		for (var i=0; i<arr.length; i++) {
+			str = str + arr[i] + ',\n';		
+		}
+		console.log(str);
+		$("#classList").text(str);		
+
+	}
+
+
+	function shuffle(array) {
+		  var currentIndex = array.length, temporaryValue, randomIndex;
+
+		while (0 !== currentIndex) {
+
+			randomIndex = Math.floor(Math.random() * currentIndex);
+			currentIndex -= 1;
+
+			temporaryValue = array[currentIndex];
+			array[currentIndex] = array[randomIndex];
+			array[randomIndex] = temporaryValue;
+		}
+
+		return array;
 	}
 
 });
