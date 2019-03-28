b7e0bd8224729b07368130692b37e48df2d63f1c
Madelyn Nelson <madnel01@protiviti.com>
2019-03-28 14:25:35 -0500

    no repeating names

--- rand.js
+++ rand.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {
 
 	//var roster = ["Alicia Weaver", "Alyssa Brasse", "Ari Cazeau", "Bing Li", "Catherine Atalig", "Clemone Reynolds", "Elaine Lau", "Erin Bouton", "Isabella Rivera", "Jessica Kukin", "Julie Margolin", "Katriona Durkin", "Leesett Nervil", "Sade Jansen", "Sanaa Mock", "Stacy Kim", "Sunjana Kulkarni", "Taylour Peters", "Tessa Sevieux", "Agnes Wong"];
-	var roster = [
+	var initial_roster = [
 		"Lily",
 		"Josh",
 		"Zaid",
@@ -14,6 +14,8 @@ $( document ).ready(function() {
 		"Reeti",
 	];
 
+	var roster = initial_roster.slice(0);
+
 	$("#go").on("click", function(){
 		getName();
 	});
@@ -24,8 +26,14 @@ $( document ).ready(function() {
 	});
 
 	function getName() {
+		if (roster.length == 0) {
+			$("#name").text("Done! No more people! :)");
+			roster = initial_roster.slice(0); // reset
+			return;
+		}
 		var num = Math.floor(Math.random() * roster.length);
 		$("#name").text(roster[num]);
+		roster.splice(num, 1); // remove that person from list
 	}
 
 	function getOrder() {
